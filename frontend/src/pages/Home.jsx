9c29cb4747977f3cfacf0bac6f75345c6cae84a7
inthrakumar <inthrak04@gmail.com>
2024-08-08 11:13:52 +0530

    Layout setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import bg from '../../public/front-page-bg.jpg';
+
+function Home() {
+  return (
+    <div
+      className={` object-cover min-h-screen bg-cover bg-repeat-y overflow-scroll bg-left-top`}
+      style={{ backgroundImage: `url("${bg}")` }}
+    ></div>
+  );
+}
+
+export default Home;
